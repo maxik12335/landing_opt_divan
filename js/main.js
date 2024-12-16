0bac6d22d636a25485f096702d6661eb339cfa44
Maksim Kuznetsov <maxim.it@bk.ru>
2024-12-16 11:52:44 +0300

    test

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ buttonsModalOpen.forEach(button => {
 modalClose.addEventListener('click', () => {
   modal.classList.contains('modal_show') ? modal.classList.remove('modal_show') : ""
 })
+
+// mobile menu
+// swiper-pagination-show
+// mobile-menu-button-hide
+const mobyleMenuButtonOpen = document.querySelector('.mobile-menu-button')
+const mobilePagination = document.querySelector('.swiper-pagination')
+
+mobyleMenuButtonOpen.addEventListener('click', () => {
+  mobyleMenuButtonOpen.classList.contains('mobile-menu-button-hide') ? mobyleMenuButtonOpen.classList.remove('mobile-menu-button-hide')
+  : mobyleMenuButtonOpen.classList.add('mobile-menu-button-hide')
+  
+  mobilePagination.classList.contains('swiper-pagination-show') ? mobilePagination.classList.remove('swiper-pagination-show') : mobilePagination.classList.add('swiper-pagination-show')
+})
+
+const mobilePaginationItems = document.querySelectorAll('.swiper-pagination-bullet')
+mobilePaginationItems.forEach(paginationItem => {
+  paginationItem.addEventListener('click', () => {
+    mobyleMenuButtonOpen.classList.contains('mobile-menu-button-hide') ? mobyleMenuButtonOpen.classList.remove('mobile-menu-button-hide')
+    : mobyleMenuButtonOpen.classList.add('mobile-menu-button-hide')
+    
+    mobilePagination.classList.contains('swiper-pagination-show') ? mobilePagination.classList.remove('swiper-pagination-show') : mobilePagination.classList.add('swiper-pagination-show')
+  })
+})
